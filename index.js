ec4afb5b47d2ee61733df61b696aca867a365ded
Tituz175 <tobititus01@gmail.com>
2022-01-26 16:18:03 +0100

    add  player button working perfectly.

--- index.js
+++ index.js
@@ -1,18 +1,107 @@
 const button = document.querySelector("button");
 const response = document.querySelector("#response");
+const playerMother = document.querySelector("#playerMother");
 let inputs;
 let firstName;
 let lastName;
 let playerCountry;
 let playerScore;
 let allfields = true;
-const playerObj = {
-  firstName: "",
-  lastName: "",
-  playerCountry: "",
-  playerScore: 0,
-  players: [],
+
+let createDiv = (divName) => {
+  let className = divName;
+  divName = document.createElement("div");
+  divName.setAttribute("class", className);
+  return divName;
 };
+
+let name,
+  date,
+  country,
+  score,
+  playerContainer,
+  playerDetails,
+  playerNametime,
+  playerScorecontroller,
+  iconDel,
+  iconPlus5,
+  iconMinus5;
+
+let elementCreates = () => {
+  name = createDiv("name");
+  date = createDiv("date");
+  country = createDiv("country");
+  score = createDiv("score");
+  playerContainer = createDiv("playerContainer");
+  playerDetails = createDiv("playerDetails");
+  playerNametime = createDiv("playerNametime");
+  playerScorecontroller = createDiv("playerScorecontroller");
+  iconDel = createDiv("iconDel");
+  iconPlus5 = createDiv("iconPlus5");
+  iconMinus5 = createDiv("iconMinus5");
+};
+
+let arrangePlayerdetails = (myfirstName, mylastName, myCountry, myScore) => {
+  elementCreates();
+  name.textContent = `${myfirstName} ${mylastName}`;
+  playerNametime.appendChild(name);
+  playerNametime.appendChild(date);
+  iconDel.textContent = "🗑️";
+  playerScorecontroller.appendChild(iconDel);
+  iconPlus5.textContent = "+5";
+  playerScorecontroller.appendChild(iconPlus5);
+  iconMinus5.textContent = "-5";
+  playerScorecontroller.appendChild(iconMinus5);
+  playerDetails.appendChild(playerNametime);
+  country.textContent = myCountry;
+  playerDetails.appendChild(country);
+  score.textContent = myScore;
+  playerDetails.appendChild(score);
+  playerDetails.appendChild(playerScorecontroller);
+  playerContainer.appendChild(playerDetails);
+  playerMother.append(playerContainer);
+};
+
+// arrangePlayerdetails();
+// arrangePlayerdetails();
+// arrangePlayerdetails();
+// arrangePlayerdetails();
+// arrangePlayerdetails();
+
+// console.log(elements.length)
+// for (let index = 0; index < myElements.length; index++) {
+//   myElements[index] = document.createElement("div");
+//   let className = myElements[index]
+//   console.log(className)
+//   myElements[index].setAttribute("class", `${className}`);
+//   // console.log(myElements[index])
+// }
+
+// 11
+let players = [];
+// const playerObj = {
+//   details: {
+//     firstName: "",
+//     lastName: "",
+//     playerCountry: "",
+//     playerScore: 0,
+//   },
+//   detailsUpdate(firstName, lastName, playerCountry, playerScore) {
+//     this.details.firstName = firstName;
+//     this.details.lastName = lastName;
+//     this.details.playerCountry = playerCountry;
+//     this.details.playerScore = playerScore;
+//     players.push(this.details);
+//   },
+//   displayDetails() {
+//     this.players.forEach((element) => {
+//       // console.log(element)
+//       name.textContent = `${this.firstName} ${lastName}`;
+//       country.textContent = `${this.playerCountry}`;
+//       score.textContent = `${this.playerScore}`;
+//     });
+//   },
+// };
 
 // class Player {
 //   constructor(firstName, lastName, playerCountry, playerScore) {
@@ -47,22 +136,23 @@ let assignValues = (array) => {
 
 let playerScoretypeCheck = () => {
   if (isNaN(parseInt(playerScore))) {
-    console.log(true);
     return true;
   } else {
-    console.log(false);
     return false;
   }
 };
 
-let saveDetails = (array) => {
+let playerObj;
+
+let saveDetails = () => {
+  playerObj = {};
+
   playerObj.firstName = firstName;
   playerObj.lastName = lastName;
   playerObj.playerCountry = playerCountry;
   playerObj.playerScore = parseInt(playerScore);
-  array.push(playerObj);
-  console.log(playerObj);
-  console.log(array);
+  players.push(playerObj);
+  playerObj = undefined;
 };
 
 let clearResponse = () => {
@@ -71,7 +161,7 @@ let clearResponse = () => {
   }, 3000);
 };
 
-
+let sameDetails = false;
 
 button.addEventListener("click", () => {
   response.style.display = "none";
@@ -79,18 +169,41 @@ button.addEventListener("click", () => {
   if (checkAllinput(inputs)) {
     assignValues(inputs);
     if (!playerScoretypeCheck()) {
-      if (playerObj.players.length > 0) {
-        playerObj.players.forEach(({ firstName, lastName }) => {
-          if (firstName == firstName && lastName == lastName) {
-            response.textContent = "Player details exist already";
-            response.style.display = "block";
-            clearResponse();
-          } else {
-            saveDetails(playerObj.players);
-          }
+      if (!(players.length >= 1)) {
+        saveDetails();
+        playerMother.textContent = "";
+        players.forEach((element) => {
+          arrangePlayerdetails(
+            element.firstName,
+            element.lastName,
+            element.playerCountry,
+            element.playerScore
+          );
         });
       } else {
-        saveDetails(playerObj.players);
+        players.forEach((element) => {
+          if (element.firstName == firstName && element.lastName == lastName) {
+            sameDetails = true;
+            clearResponse();
+          }
+        });
+        if (!sameDetails) {
+          saveDetails();
+          playerMother.textContent = "";
+          players.forEach((element) => {
+            arrangePlayerdetails(
+              element.firstName,
+              element.lastName,
+              element.playerCountry,
+              element.playerScore
+            );
+          });
+        } else {
+          response.textContent = "Player details exist already";
+          response.style.display = "block";
+          clearResponse();
+          sameDetails = false;
+        }
       }
     } else {
       response.textContent = "Player score must be a numerical value";
